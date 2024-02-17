0f02005497e35f8ce6889141047f63fef25f1277
thiagofelizardo <thiagofelizardo2@gmail.com>
2024-02-17 16:49:07 -0300

    comit

---
+++ Java Programacao Orientada a Objetos Projetos-Nelio Alves-2023/src/main/java/br/estudos/com/ENUMS/Program.java
@@ -0,0 +1,22 @@
+package br.estudos.com.ENUMS;
+
+import br.estudos.com.ENUMS.entities.Order;
+import br.estudos.com.ENUMS.entities.OrderStatus;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+    }
+}
---
+++ Java Programacao Orientada a Objetos Projetos-Nelio Alves-2023/src/main/java/br/estudos/com/ENUMS/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package br.estudos.com.ENUMS.entities;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+
+}
